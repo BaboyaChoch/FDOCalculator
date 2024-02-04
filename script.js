fce28e6a30a2b8b7b722dbd17d30f968741442b5
Dara7K <136499907+Dara7K@users.noreply.github.com>
2024-02-03 22:35:50 -0600

    Update script.js
    added increment and increment start

--- script.js
+++ script.js
@@ -7,10 +7,14 @@ const inputError = document.getElementById("error");
 const resultsTable = document.getElementById("results-table");
 const RESULTS_HEADER = resultsTable.innerHTML;
 
+const getStep = () => document.getElementById("step").value;
+
+const getIncrementStart = () => document.getElementById("IncrementStart").value;
+
 const getUnit = () => document.querySelector('input[name="unit"]:checked').value;
 
 // @TODO write function to call a script or api to get the calculation results
-const getResults = (diameter1, diameter2) => FDOCalcualtor.calculate(diameter1, diameter2, getUnit());
+const getResults = (diameter1, diameter2) => FDOCalcualtor.calculate(diameter1, diameter2, getUnit(),getIncrementStart(),getStep());
 
 // aux function to create the table rows of results
 const createTableRows = (results) => {
@@ -23,8 +27,8 @@ const createTableRows = (results) => {
 
 // whem calculate button is clicked, get the values from the inputs and call the function to get the results and display them
 const onCalculate = (event) => {
-	const diameter1 = document.getElementById("diameter_1").value;
-	const diameter2 = document.getElementById("diameter_2").value;
+	const diameter1 = parseInt(document.getElementById("diameter_1").value);
+	const diameter2 = parseInt(document.getElementById("diameter_2").value);
 
 	if (diameter1 === "" || diameter2 === "") return;
 
